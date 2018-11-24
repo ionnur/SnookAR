834f817fe9ae825a83cd0cdeea9860e8e1a52521
Maximum Gale <kieran.gale@icloud.com>
2018-11-24 19:06:51 +0000

    New pathfinding for balls

---
+++ SnookAR/Assets/Scripts/Pathfinding/BallPathfinding.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BallPathfinding : MonoBehaviour {
+
+	/* BallPathfinding CLASS
+	 * 
+	 * Follows a preset path (and returns to it
+	 * from the point it left) while also
+	 * allowing the player to interact with
+	 * the ball's position.
+	 * 
+	 * -Kieran
+	 * */
+
+	//Holds all the pathfinding vectors.
+	private PathfindingMaster master;
+
+	//The next point the ball is moving toward
+	//(in terms of master's path).
+	private int pathfindingIndex = 0;
+
+	//The "emergency backup" path the ball will
+	//use to get back to the path.
+	private List<Vector3> path;
+
+	private bool useDefaultPath = true;
+
+	private Rigidbody rb;
+
+
+    public static bool currentlyHit;
+
+    //How much force is applied to the ball
+    //each frame to "steer" it.
+    [SerializeField]
+
+	private float force = 1;
+    private float weight = 1;
+
+
+    void Start() {
+		master = FindObjectOfType<PathfindingMaster>();
+		rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * 30, ForceMode.Impulse);
+        currentlyHit = false;
+	}
+
+	void FixedUpdate() {
+		//Check for changes in pathfinding node.
+		if(useDefaultPath) {
+			if((master.mainPath[pathfindingIndex] - transform.position).sqrMagnitude < 3) {
+				useDefaultPath = true;
+				//Move to the next pathfinding index.
+				if(pathfindingIndex < master.mainPath.Count - 1) pathfindingIndex++;
+				else {
+					//Do something important involving
+					//despawning etc.
+				}
+			}
+		} else {
+			if((path[0] - transform.position).sqrMagnitude < 3) {
+				//Move to the next pathfinding index.
+				path.RemoveAt(0);
+			}
+		}
+
+        //Move towards the next node.
+		if(rb.velocity.magnitude > 0.1f) {
+			currentlyHit = false;
+		} else if(rb.velocity.magnitude > 3f) {
+			currentlyHit = true;
+			useDefaultPath = false;
+			path.Clear();
+		}
+
+        if (currentlyHit == false)
+        {
+			if(useDefaultPath) {
+				//We can use the default path -
+				//keep moving along it.
+				rb.AddForce((master.mainPath[pathfindingIndex] - transform.position).normalized * force);
+			} else {
+				//We can't use the default path.
+				//Get a new path leading towards
+				//the correct node.
+				if(path.Count == 0) {
+					path = master.AStar(transform.position, master.mainPath[pathfindingIndex]);
+				}
+				else rb.AddForce((path[0] - transform.position).normalized * force);
+			}
+        }
+
+	}
+
+}
---
+++ SnookAR/Assets/Scripts/Pathfinding/PathfindingMaster.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq.Expressions;
+
+public class PathfindingMaster : MonoBehaviour {
+
+	/* PathfindingMaster CLASS
+	 * 
+	 * You should only have one of these
+	 * in the scene at once.
+	 * 
+	 * -Kieran
+	 * */
+
+	[SerializeField]
+	private Transform startPoint;
+
+	[SerializeField]
+	private Transform endPoint;
+
+
+	private const float NODE_SCALE = 0.5f;
+
+	private const int TABLE_WIDTH = 40;
+	private const int TABLE_DEPTH = 80;
+
+	private const int WIDTH_OFFSET = 10;
+	private const int DEPTH_OFFSET = 20;
+
+	//false by default. True if there's an obstacle
+	//blocking the balls.
+	public bool[,] pathfindingBlocked = new bool[TABLE_WIDTH, TABLE_DEPTH];
+
+	//Arrays used by A*.
+	private Vector2Int[,] previousNodes = new Vector2Int[TABLE_WIDTH, TABLE_DEPTH];
+	private bool[,] visitedNodes = new bool[TABLE_WIDTH, TABLE_DEPTH];
+	private float[,] startDists = new float[TABLE_WIDTH, TABLE_DEPTH];
+	private float[,] endDists = new float[TABLE_WIDTH, TABLE_DEPTH];
+
+	//The positions of each path point.
+	public List<Vector3> mainPath = new List<Vector3>();
+
+	/// <summary>
+	/// Carve the pathfinding grid where an obstacle was placed.
+	/// </summary>
+	/// <param name="worldCentre">World centre of the obstacle.</param>
+	/// <param name="dimensions">Radial dimensions of the object (m).</param>
+	public void Carve(Vector3 worldCentre, Vector2 dimensions) {
+		//Convert worldCentre into an index
+		//of the pathfinding array.
+		Vector2Int localCentre = GlobalToLocal(worldCentre);
+
+		Vector2Int gridDimensions = new Vector2Int((int)(dimensions.x / NODE_SCALE), (int)(dimensions.y / NODE_SCALE));
+
+		//Carve the pathfinding array at the
+		//locations below the obstacle.
+		for(int x = localCentre.x - gridDimensions.x; x <= localCentre.x + gridDimensions.x; x++) {
+			for(int y = localCentre.y - gridDimensions.y; y <= localCentre.y + gridDimensions.y; y++) {
+				//Ignore areas outside of the array.
+				if(x < 0 || x >= TABLE_WIDTH || y < 0 || y >= TABLE_DEPTH) continue;
+
+				pathfindingBlocked[x, y] = true;
+			}
+		}
+	}
+
+	private Vector2Int GlobalToLocal(Vector3 vecIn) {
+		return new Vector2Int((int)(vecIn.x / NODE_SCALE) + WIDTH_OFFSET, (int)(vecIn.z / NODE_SCALE) + DEPTH_OFFSET);
+	}
+
+	private Vector3 LocalToGlobal(Vector2Int vecIn) {
+		return new Vector3((vecIn.x - WIDTH_OFFSET) * NODE_SCALE, 0, (vecIn.y - DEPTH_OFFSET) * NODE_SCALE);
+	}
+
+	//Runs A* between two grid points,
+	//returning each corner.
+	private List<Vector2Int> InternalAStar(Vector2Int start, Vector2Int end) {
+		List<Vector2Int> pathNodes = new List<Vector2Int>();
+		List<Vector2Int> openNodes = new List<Vector2Int>();
+
+		int nextIndex;
+		float shortestDist;
+		Vector2Int currentNode;
+		bool impassable;
+
+		//Initialise the A* arrays.
+		for(int i = 0; i < TABLE_WIDTH; i++) {
+			for(int j = 0; j < TABLE_DEPTH; j++) {
+				previousNodes[i, j] = Vector2Int.one * -1;
+				visitedNodes[i, j] = false;
+				startDists[i, j] = float.PositiveInfinity;
+				endDists[i, j] = float.PositiveInfinity;
+			}
+		}
+
+		//Begin at the start.
+		openNodes.Add(start);
+		startDists[start.x, start.y] = 0;
+		endDists[start.x, start.y] = (start - end).sqrMagnitude;
+
+		while(openNodes.Count > 0) {
+			//Main A* loop.
+			//Find the node within openNodes with
+			//the lowest heuristic distance to the end.
+			shortestDist = float.PositiveInfinity;
+			nextIndex = 0;
+			for(int i = 0; i < openNodes.Count; i++) {
+				if(endDists[openNodes[i].x, openNodes[i].y] < shortestDist) {
+					shortestDist = endDists[openNodes[i].x, openNodes[i].y];
+					nextIndex = i;
+				}
+			}
+			currentNode = openNodes[nextIndex];
+
+			//Check if this node is the target.
+			if(currentNode == end) {
+				//Found the target. Return the path
+				//leading to the target.
+				List<Vector2Int> returnPath = new List<Vector2Int>();
+				currentNode = end;
+				while(previousNodes[currentNode.x, currentNode.y] != Vector2Int.one * -1) {
+					returnPath.Insert(0, currentNode);
+					currentNode = previousNodes[currentNode.x, currentNode.y];
+				}
+				return returnPath;
+			};
+
+			//Count this node as visited.
+			openNodes.Remove(currentNode);
+			visitedNodes[currentNode.x, currentNode.y] = true;
+
+			//Visit the node by checking all of its
+			//neighbours in turn.
+			//Neighbours are 2 nodes away (A*16)
+			//to yield smoother vectors.
+			for(int x = -2; x <= 2; x++) {
+				for(int y = -2; y <= 2; y++) {
+					//Ignore non-edge nodes.
+					if(Mathf.Abs(x) < 2 && Mathf.Abs(y) < 2) continue;
+					//Ignore nodes out of index.
+					if(currentNode.x + x < 0 || currentNode.x + x > TABLE_WIDTH || 
+						currentNode.y + y < 0 || currentNode.y + y > TABLE_DEPTH) continue;
+					//Check if the node was already visited.
+					if(visitedNodes[currentNode.x + x, currentNode.y + y] == true) continue;
+
+					//Check if the node, or any of the 8 spaces
+					//adjacent to it, is impassable.
+					impassable = false;
+					for(int x2 = -1; x2 <= 1; x2++) {
+						for(int y2 = -1; y2 <= 1; y2++) {
+							//Ignore nodes out of index.
+							if(currentNode.x + x + x2 < 0 || currentNode.x + x + x2 > TABLE_WIDTH || 
+								currentNode.y + y + y2 < 0 || currentNode.y + y + y2 > TABLE_DEPTH) continue;
+
+							if(pathfindingBlocked[currentNode.x + x + x2, currentNode.y + y + y2]) {
+								//Path blocked. Consider this node visited.
+								visitedNodes[currentNode.x + x, currentNode.y + y] = true;
+								impassable = true;
+							}
+						}
+					}
+					if(impassable) return new List<Vector2Int>();
+
+					//If the node can be passed through,
+					//and hasn't already been put in the open nodes,
+					//add it to the open nodes.
+					if(!openNodes.Contains(new Vector2Int(currentNode.x + x, currentNode.y + y))) {
+						openNodes.Add(new Vector2Int(currentNode.x + x, currentNode.y + y));
+					}
+
+					//Update the heuristics for this node.
+					float dist = startDists[currentNode.x, currentNode.y] + (x * x) + (y * y);
+					if(dist < startDists[currentNode.x + x, currentNode.y + y]) {
+						startDists[currentNode.x + x, currentNode.y + y] = dist;
+						endDists[currentNode.x + x, currentNode.y + y] = dist + (currentNode + new Vector2Int(x, y) - end).sqrMagnitude;
+						previousNodes[currentNode.x + x, currentNode.y + y] = currentNode;
+					}
+				}
+			}
+		}
+		return new List<Vector2Int>();
+	}
+
+	//Wrapper for InternalAStar that uses world co-ordinates.
+	public List<Vector3> AStar(Vector3 startPos, Vector3 endPos) {
+		List<Vector2Int> tmp = InternalAStar(GlobalToLocal(startPos), GlobalToLocal(endPos));
+
+		List<Vector3> returnList = new List<Vector3>();
+		while(tmp.Count > 0) {
+			returnList.Add(LocalToGlobal(tmp[0]));
+			tmp.RemoveAt(0);
+		}
+		return returnList;
+	}
+
+	// Use this for initialization
+	void Start () {
+		mainPath = AStar(Vector3.zero, Vector3.right * 5);
+		for(int i = 0; i < mainPath.Count; i++) {
+			Debug.Log(mainPath[i]);
+		}
+		Debug.Log(GlobalToLocal(Vector3.zero));
+		Debug.Log(LocalToGlobal(new Vector2Int(10, 20)));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
